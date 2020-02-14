bdecc6c7b6020e31ed180a84742b784db3990e45
Std1o <37378410+Std1o@users.noreply.github.com>
2020-02-14 19:57:58 +0300

    0.0.1

--- app/src/main/java/com/stdio/intentanimations/MainActivity.java
+++ app/src/main/java/com/stdio/intentanimations/MainActivity.java
@@ -2,7 +2,11 @@ package com.stdio.intentanimations;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.blogspot.atifsoftwares.animatoolib.Animatoo;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,56 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        startActivity(new Intent(this, TestActivity.class));
+        switch (view.getId()) {
+            case R.id.button:
+                Animatoo.animateZoom(this);
+                break;
+            case R.id.button2:
+                Animatoo.animateFade(this);
+                break;
+            case R.id.button3:
+                Animatoo.animateWindmill(this);
+                break;
+            case R.id.button4:
+                Animatoo.animateSpin(this);
+                break;
+            case R.id.button5:
+                Animatoo.animateDiagonal(this);
+                break;
+            case R.id.button6:
+                Animatoo.animateSplit(this);
+                break;
+            case R.id.button7:
+                Animatoo.animateShrink(this);
+                break;
+            case R.id.button8:
+                Animatoo.animateCard(this);
+                break;
+            case R.id.button9:
+                Animatoo.animateInAndOut(this);
+                break;
+            case R.id.button10:
+                Animatoo.animateSwipeLeft(this);
+                break;
+            case R.id.button11:
+                Animatoo.animateSwipeRight(this);
+                break;
+            case R.id.button12:
+                Animatoo.animateSlideLeft(this);
+                break;
+            case R.id.button13:
+                Animatoo.animateSlideRight(this);
+                break;
+            case R.id.button14:
+                Animatoo.animateSlideDown(this);
+                break;
+            case R.id.button15:
+                Animatoo.animateSlideUp(this);
+                break;
+        }
     }
 }
